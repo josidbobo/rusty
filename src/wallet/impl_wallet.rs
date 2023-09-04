0e121a778367926f6f52c7760e20a240fe003b4c
Joshua Okoro <josidbobo@gmail.com>
2023-09-04 17:08:46 +0100

    fix: sql server

--- src/wallet/impl_wallet.rs
+++ src/wallet/impl_wallet.rs
@@ -1,15 +1,29 @@
-use bdk::{Wallet, bitcoin::Network, database::MemoryDatabase, SyncOptions, wallet::AddressIndex::New, blockchain::ElectrumBlockchain, electrum_client::Client};
+use std::{net::SocketAddr, path::Path};
+#[tokio::main]
+use bdk::{Wallet, bitcoin::Network, database::SqliteDatabase, SyncOptions, wallet::AddressIndex::New, blockchain::ElectrumBlockchain, electrum_client::Client};
 use super::read_env::env_settings;
 
 pub fn wallet_implementation() -> anyhow::Result<()>{
+    let my_path = Path::new("paypaul.db");
+
     let descriptor = env_settings()?;
-    let wallet = Wallet::new(&descriptor, None, Network::Testnet, MemoryDatabase::default())?;
+    let wallet = Wallet::new(
+        &descriptor, None, 
+        Network::Testnet, SqliteDatabase::new(my_path), 
+    )?;
+
     let blockchain = ElectrumBlockchain::from(Client::new("ssl://electrum.blockstream.info:60002")?);
-    
-    wallet.sync(&blockchain, SyncOptions::default())?;
 
     let balance = wallet.get_balance()?;
     let address = wallet.get_address(New)?;
+
+    let app = Router::new().route("/", get(handler));
+
+    let addr: SocketAddr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on {}", addr);
+
+    axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
+    wallet.sync(&blockchain, SyncOptions::default())?;
 
     Ok(())
 
--- target/debug/deps/rusty.d
+++ target/debug/deps/rusty.d
@@ -1,7 +1,8 @@
-C:\Users\OKORO-JOSHUA\Desktop\Applications\Dapps\rusty\target\debug\deps\rusty.exe: src\main.rs src\wallet\mod.rs src\wallet\impl_wallet.rs
+C:\Users\OKORO-JOSHUA\Desktop\Applications\Dapps\rusty\target\debug\deps\rusty.exe: src\main.rs src\wallet\mod.rs src\wallet\impl_wallet.rs src\wallet\read_env.rs
 
-C:\Users\OKORO-JOSHUA\Desktop\Applications\Dapps\rusty\target\debug\deps\rusty.d: src\main.rs src\wallet\mod.rs src\wallet\impl_wallet.rs
+C:\Users\OKORO-JOSHUA\Desktop\Applications\Dapps\rusty\target\debug\deps\rusty.d: src\main.rs src\wallet\mod.rs src\wallet\impl_wallet.rs src\wallet\read_env.rs
 
 src\main.rs:
 src\wallet\mod.rs:
 src\wallet\impl_wallet.rs:
+src\wallet\read_env.rs:
