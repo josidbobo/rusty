8cf681d3a0ecb261f0889011bd9bef946378bf78
Joshua Okoro <josidbobo@gmail.com>
2023-09-04 13:00:26 +0100

    chore: wallet key

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::env;
+use bdk::{Wallet, bitcoin::Network, database::MemoryDatabase};
 use dotenv::from_filename;
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+fn main() -> anyhow::Result<()>{
     from_filename(".env").ok();
 
     let descriptor = env::var("WALLET_DESCRIPTOR")?;
 
-    dbg!(descriptor);
+    dbg!(&descriptor);
 
+    let wallet = Wallet::new(&descriptor, None, Network::Testnet, MemoryDatabase::default())?;
+
+    dbg!(wallet);
     Ok(())
 
 }
